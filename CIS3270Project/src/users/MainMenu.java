1aa9c4a8f1670be95e9a85df20d2b1d392d16f3e
superice123 <qle3@student.gsu.edu>
2015-04-25 15:55:59 -0400

    I let user retrieved password and create splash screen

--- CIS3270Project/src/users/MainMenu.java
+++ CIS3270Project/src/users/MainMenu.java
@@ -1,12 +1,15 @@
 package users;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
+import java.net.MalformedURLException;
+import java.net.URL;
 import javax.swing.*;
 
 public class MainMenu extends JFrame {
     private final JButton login = new JButton("Login");
     private final JButton register = new JButton("Register");
-    private final JButton Main = new JButton("Main");
+    private final JButton Main1 = new JButton("Main");
+    private final JButton Main2 = new JButton("Main");
     private final JButton logcustomer = new JButton("Customer");
     private final JButton logad= new JButton("Administrator");
     private final JButton regcustomer = new JButton("Customer");
@@ -22,17 +25,22 @@ public class MainMenu extends JFrame {
    final JPanel log = new JPanel(new FlowLayout(FlowLayout.CENTER,2,2));
     log.add(logcustomer);
     log.add(logad);
-    log.add(Main);
+    log.add(Main1);
     log.setVisible(false);
     
     
     final JPanel regist = new JPanel(new FlowLayout(FlowLayout.CENTER,2,2));
     regist.add(regcustomer);
     regist.add(regad);
-    regist.add(Main);
+    regist.add(Main2);
     regist.setVisible(false);
     
-    Main.addActionListener((ActionEvent ev) -> {
+    Main1.addActionListener((ActionEvent ev) -> {
+        panel.setVisible(true);
+        log.setVisible(false);
+        regist.setVisible(false);
+    });
+    Main2.addActionListener((ActionEvent ev) -> {
         panel.setVisible(true);
         log.setVisible(false);
         regist.setVisible(false);
@@ -81,7 +89,19 @@ public class MainMenu extends JFrame {
     });
     }
     
-public static void main(String[] agrs){    
+public static void main(String[] agrs){
+    
+    JWindow window = new JWindow();
+    try{
+    window.getContentPane().add(
+    new JLabel("", new ImageIcon(new URL("http://cdn.makeagif.com/media/4-25-2015/LyuNvb.gif")), SwingConstants.CENTER));
+    window.setBounds(400, 150, 630, 290);
+    window.setVisible(true);
+
+    Thread.sleep(6000);
+        } catch (InterruptedException | MalformedURLException e) {
+    }
+        window.setVisible(false);
     JFrame frame = new MainMenu();
     frame.setTitle("Main Menu");
     frame.setSize(800, 600);
