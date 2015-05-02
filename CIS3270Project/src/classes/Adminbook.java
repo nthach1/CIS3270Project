5591c9be4fd423154577a772965257387b9bec11
superice123 <qle3@student.gsu.edu>
2015-05-02 15:46:01 -0400

    Add comment

--- CIS3270Project/src/classes/Adminbook.java
+++ CIS3270Project/src/classes/Adminbook.java
@@ -6,9 +6,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -20,26 +18,21 @@ import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
 
 public final class Adminbook extends JFrame {
-	private final String[] column = { "Book", "Flightnumber", "Datetime",
+        // Create data and header for table
+	private final String[] column = {"Flightnumber", "Datetime",
 			"Departure", "Destination", "SeatAvailable", "SeatOccupied" };
 	private final Object[][] data = new Object[20][30];
 	private final JTextField departure = new JTextField();
 	private final JButton exDeparture = new JButton("Find");
-	ArrayList<JCheckBox> checkbox = new ArrayList<>(20);
 	private final JTable jTable = new JTable(data, column) {
+        // Made cell of table unchangable    
 		@Override
 		public boolean isCellEditable(int row, int column) {
-			return true;
-		}
-
-		@Override
-		public Class getColumnClass(int column) {
-			return Boolean.class;
+			return false;
 		}
 	};
-	private final TableRowSorter<TableModel> sorter = new TableRowSorter<>(
-			jTable.getModel());
-
+	private final TableRowSorter<TableModel> sorter = new TableRowSorter<>(jTable.getModel());
+        //Establish connection
 	public void connection() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
@@ -53,37 +46,37 @@ public final class Adminbook extends JFrame {
 			// Execute a statement
 			ResultSet resultSet = statement
 					.executeQuery("select * from flight");
-			for (int j = 0; j < data.length; j++) {
-				checkbox.add(new JCheckBox());
-			}
+			// value will be save
 			for (int i = 0; i < data.length; i++) {
-				while (resultSet.next()) {
-					data[i][0] = checkbox.get(i);
-					data[i][1] = resultSet.getString(1);
-					data[i][2] = resultSet.getString(2);
-					data[i][3] = resultSet.getString(3);
-					data[i][4] = resultSet.getString(4);
-					data[i][5] = resultSet.getString(5);
-					data[i][6] = resultSet.getString(6);
+				while (resultSet.next()) {				
+					data[i][0] = resultSet.getString(1);
+					data[i][1] = resultSet.getString(2);
+					data[i][2] = resultSet.getString(3);
+					data[i][3] = resultSet.getString(4);
+					data[i][4] = resultSet.getString(5);
+					data[i][5] = resultSet.getString(6);
 				}
 			}
 			connection.close();
 		} catch (ClassNotFoundException | SQLException ex) {
 		}
 	}
-
-	public Adminbook() {
+public Adminbook() {
 		connection();
+                //Add table to JScrollPane
 		add(new JScrollPane(jTable));
+                //Allow sort in row
 		jTable.setRowSorter(sorter);
 		JPanel panel = new JPanel(new java.awt.BorderLayout());
+                //Add Label Flight
 		panel.add(new JLabel("Flight "), BorderLayout.WEST);
+                //Add textfield and button to panel
 		panel.add(departure, BorderLayout.CENTER);
 		panel.add(exDeparture, BorderLayout.EAST);
-
+                //Add Jpanel to current class
 		add(panel, BorderLayout.SOUTH);
 		add(new JScrollPane(jTable), BorderLayout.CENTER);
-
+                // Assign listener to button execution
 		exDeparture.addActionListener((java.awt.event.ActionEvent e) -> {
 			String text = departure.getText();
 			if (text.trim().length() == 0)
@@ -92,4 +85,5 @@ public final class Adminbook extends JFrame {
 				sorter.setRowFilter(RowFilter.regexFilter(text));
 		});
 	}
+
 }
